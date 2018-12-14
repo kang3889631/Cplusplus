8630b060cfb26a1d4a5d7abfd3432bc598811a0d
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2018-12-13 20:52:28 -0800

    Copy constructor practice: Sample(const Sample &p) {}

---
+++ week3_Q2/week3_Q2/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Sample {
+public:
+	int v;
+	Sample(int n) :v(n) { }
+	// 在此处补充你的代码
+public:
+	Sample(const Sample &p) { v = p.v*2; }
+};
+
+int main() {
+	Sample a(5);
+	Sample b = a;
+	cout << b.v;
+	return 0;
+}
