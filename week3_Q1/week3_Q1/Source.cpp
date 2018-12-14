4755f4f2605a58e829d979dcf540dd2c46c0afda
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2018-12-13 20:24:38 -0800

    The "&" denotes a reference instead of a pointer to an object

--- week3_Q1/week3_Q1/Source.cpp
+++ week3_Q1/week3_Q1/Source.cpp
@@ -1,9 +1,18 @@
-class Test {
+#include <iostream>
+using namespace std;
+
+class A {
 public:
-	Test(int n) {}       //1
-	Test(int n,int m){}  //2
-	Test(){}             //3
+	int val;
+	A(int n=0) { val = n; }
+	A &GetObj() {
+		return *this;
+	}
 };
-Test array1[3] = { 1,Test(1,2) }; //1,2,3
-Test array2[3] = { Test(2,3),Test(1,2),1 }; //2,2,1
-Test *pArray[3] = { new Test(4),new Test(1,2) };//1,2
+
+int main() {
+	A a;
+	cout << a.val << endl;
+	a.GetObj() = 5;
+	cout << a.val << endl;
+}
