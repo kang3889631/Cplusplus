26a56882926df18f3f8b9b1a5e9ca1c094d5d7b8
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2018-12-15 15:56:24 -0800

    first build class--finally

--- wee3_Q4/wee3_Q4/Source.cpp
+++ wee3_Q4/wee3_Q4/Source.cpp
@@ -2,117 +2,216 @@
 #include <iomanip>
 using namespace std;
 
-class Red {
+class Warrior {
 	int m_dragon = 0;
 	int m_ninja = 0;
 	int m_iceman = 0;
 	int m_lion = 0;
 	int m_wolf = 0;
-	int count = 0;
 
 public:
-	Red(int d, int n, int i, int l, int w, int k) :m_dragon(d), m_ninja(n), m_iceman(i), m_lion(l), m_wolf(w),m(k) {}
+	Warrior(int d, int n, int i, int l, int w, int k) :m_dragon(d), m_ninja(n), m_iceman(i), m_lion(l), m_wolf(w), m(k) {}
+	int count = 0;
 	int m = 0;
-	static int dragon;
-	static int ninja;
-	static int iceman;
-	static int lion;
-	static int wolf;
+	int dragon = 0;
+	int ninja = 0;
+	int iceman = 0;
+	int lion = 0;
+	int wolf = 0;
 
 	int build_warrior(int i);
+	int build_warrior_blue(int i);
 };
 
-int Red::dragon = 0;
-int Red::ninja = 0;
-int Red::iceman = 0;
-int Red::lion = 0;
-int Red::wolf = 0;
-
-int Red::build_warrior(int i) {
+int Warrior::build_warrior(int i) {
 	int a = 0;
 	switch (i) {
-	case 0: 
-		if (m < m_iceman){
-			build_warrior(1);
+	case 0:
+		if (m < m_iceman) {
+			return build_warrior(1);
 		}
-			iceman++;
-			m -= m_iceman;
-			count++;
-			cout << setfill('0') << setw(3) << count - 1 << " ";
-			cout << "red iceman " << count << " born with strenth " << m_iceman << "," << iceman << " iceman in red headquarter" << endl;
-			a = 1;
-			break;
+		iceman++;
+		m -= m_iceman;
+		count++;
+		cout << "red iceman " << count << " born with strength " << m_iceman << "," << iceman << " iceman in red headquarter" << endl;
+		a = 1;
+		break;
 
 	case 1:
 		if (m < m_lion) {
-			build_warrior(2);
+			return build_warrior(2);
 		}
-			lion++;
-			m -= m_lion;
-			count++;
-			cout << setfill('0') << setw(3) << count - 1 << " ";
-			cout << "lion " << count << " born with strenth " << m_lion << "," << lion << " lion in red headquarter" << endl;
-			a = 2;
-			break;
+		lion++;
+		m -= m_lion;
+		count++;
+		cout << "red lion " << count << " born with strength " << m_lion << "," << lion << " lion in red headquarter" << endl;
+		a = 2;
+		break;
 
 	case 2:
 		if (m < m_wolf) {
-			build_warrior(3);
+			return build_warrior(3);
 		}
-			wolf++;
-			m -= m_wolf;
-			count++;
-			cout << setfill('0') << setw(3) << count - 1 << " ";
-			cout << "wolf " << count << " born with strenth " << m_wolf << "," << wolf << " wolf in red headquarter" << endl;
-			a = 3;
-			break;
+		wolf++;
+		m -= m_wolf;
+		count++;
+		cout << "red wolf " << count << " born with strength " << m_wolf << "," << wolf << " wolf in red headquarter" << endl;
+		a = 3;
+		break;
 
 	case 3:
 		if (m < m_ninja) {
-			build_warrior(4);
+			return build_warrior(4);
 		}
-			ninja++;
-			m -= m_ninja;
-			count++;
-			cout << setfill('0') << setw(3) << count - 1 << " ";
-			cout << "ninja " << count << " born with strenth " << m_ninja << "," << ninja << " ninja in red headquarter" << endl;
-			a = 4;
-			break;
+		ninja++;
+		m -= m_ninja;
+		count++;
+		cout << "red ninja " << count << " born with strength " << m_ninja << "," << ninja << " ninja in red headquarter" << endl;
+		a = 4;
+		break;
 
 	case 4:
 		if (m < m_dragon) {
-			build_warrior(0);
+			return build_warrior(0);
 		}
-			dragon++;
-			m -= m_dragon;
-			count++;
-			cout << setfill('0') << setw(3) << count - 1 << " ";
-			cout << "dragon " << count << " born with strenth " << m_dragon << "," << dragon << " dragon in red headquarter" << endl;
-			a = 0;
-			break;
-	case 5:
-			build_warrior(5);
-			count++;
-			cout << setfill('0') << setw(3) << count - 1 << " ";
-			cout << "red headquater stops making warriors" << endl;
-			break;
-			
+		dragon++;
+		m -= m_dragon;
+		count++;
+		cout << "red dragon " << count << " born with strength " << m_dragon << "," << dragon << " dragon in red headquarter" << endl;
+		a = 0;
+		break;
 	}
 	return a;
 }
-int main() {
-	int ca, n = 0; //ca:case number| n is health
-	int l[5] = {};
-	cin >> ca;
-	while (ca!= 0) {
-		cin >> n >> l[0] >> l[1] >> l[2] >> l[3] >> l[4];
-		
-		cout << "Case:" << ca << endl;
-		Red red(l[0], l[1], l[2], l[3], l[4], n);
-		int temp = 0;
-		while (red.m > 0) {
-			temp = red.build_warrior(temp);
+int Warrior::build_warrior_blue(int i) {
+	int a = 0;
+	switch (i) {
+	case 0:
+		if (m < m_lion) {
+			return build_warrior_blue(1);
 		}
-		ca--;
+		lion++;
+		m -= m_lion;
+		count++;
+		cout << "blue lion " << count << " born with strength " << m_lion << "," << lion << " lion in blue headquarter" << endl;
+		a = 1;
+		break;
+
+	case 1:
+		if (m < m_dragon) {
+			return build_warrior_blue(2);
+		}
+		dragon++;
+		m -= m_dragon;
+		count++;
+		cout << "blue dragon " << count << " born with strength " << m_dragon << "," << dragon << " dragon in blue headquarter" << endl;
+		a = 2;
+		break;
+
+	case 2:
+		if (m < m_ninja) {
+			return build_warrior_blue(3);
+		}
+		ninja++;
+		m -= m_ninja;
+		count++;
+		cout << "blue ninja " << count << " born with strength " << m_ninja << "," << ninja << " ninja in blue headquarter" << endl;
+		a = 3;
+		break;
+
+	case 3:
+		if (m < m_iceman) {
+			return build_warrior_blue(4);
+		}
+		iceman++;
+		m -= m_iceman;
+		count++;
+		cout << "blue iceman " << count << " born with strength " << m_iceman << "," << iceman << " iceman in blue headquarter" << endl;
+		a = 4;
+		break;
+
+	case 4:
+		if (m < m_wolf) {
+			return build_warrior_blue(0);
+		}
+		wolf++;
+		m -= m_wolf;
+		count++;
+		cout << "blue wolf " << count << " born with strength " << m_wolf << "," << wolf << " wolf in blue headquarter" << endl;
+		a = 0;
+		break;
+
+	}
+	return a;
+}
+int min(int d, int n, int i, int l, int w) {
+	int a = d;
+	if (n < a)
+		a = n;
+	if (i < a)
+		a = i;
+	if (l < a)
+		a = l;
+	if (w < a)
+		a = w;
+	return a;
+}
+int main() {
+	int ca, n = 0, count = 1; //ca:case number| n is health
+	int l[5] = {};
+
+	cin >> ca;
+	while (ca >= count) {
+		cin >> n >> l[0] >> l[1] >> l[2] >> l[3] >> l[4];
+		cout << "Case:" << count << endl;
+		Warrior red(l[0], l[1], l[2], l[3], l[4], n);
+		Warrior blue(l[0], l[1], l[2], l[3], l[4], n);
+		int r = 0, b = 0, time = 0;
+		bool red_finish_printed = false;
+		bool blue_finish_printed = false;
+		int m = min(l[0], l[1], l[2], l[3], l[4]);
+		if (red.m < m && blue.m < m) {
+			cout << setfill('0') << setw(3) << time << " ";
+			cout << "red headquarter stops making warriors" << endl;
+			cout << setfill('0') << setw(3) << time << " ";
+			cout << "blue headquarter stops making warriors" << endl;
+		}
+		while (red.m >= m||blue.m >= m) {
+			if (red_finish_printed == false && red.m < m) {
+				cout << setfill('0') << setw(3) << time << " ";
+				cout << "red headquarter stops making warriors" << endl;
+				red_finish_printed = true;
+			}
+			else if (red.m >= m) {
+				cout << setfill('0') << setw(3) << time << " ";
+				r = red.build_warrior(r);
+			}
+
+			if (blue_finish_printed == false && blue.m < m) {
+				cout << setfill('0') << setw(3) << time << " ";
+				cout << "blue headquarter stops making warriors" << endl;
+				blue_finish_printed = true;
+			}
+			else if (blue.m >= m){
+				cout << setfill('0') << setw(3) << time << " ";
+				b = blue.build_warrior_blue(b);
+			}
+			time++;
+			if (red_finish_printed == true && blue.m < m) {
+				cout << setfill('0') << setw(3) << time << " ";
+				cout << "blue headquarter stops making warriors" << endl;
+			}
+			if (blue_finish_printed == true && red.m < m) {
+				cout << setfill('0') << setw(3) << time << " ";
+				cout << "red headquarter stops making warriors" << endl;
+			}
+			if (red.m < m&&red_finish_printed == false&& blue_finish_printed==false&& blue.m < m) {
+				cout << setfill('0') << setw(3) << time << " ";
+				cout << "red headquarter stops making warriors" << endl;
+				cout << setfill('0') << setw(3) << time << " ";
+				cout << "blue headquarter stops making warriors" << endl;
+			}
+		}
+		count++;
 	}
 }
