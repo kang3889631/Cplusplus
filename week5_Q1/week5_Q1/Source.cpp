d05412af4444d801f379c7d9214b619452db28cd
shiyin weng <kang3889631@gmail.com>
2018-12-20 15:54:58 -0800

    new create

--- week5_Q1/week5_Q1/Source.cpp
+++ week5_Q1/week5_Q1/Source.cpp
@@ -1,51 +1,41 @@
-#include <iostream>
-#include <string>
+﻿#include <iostream>
+#include <cstring>
+#include <cstdlib>
 using namespace std;
-class CStudent {
-private:
-	string name;
-	string id;
-	char gender;
-	int age;
-public:
-	void PrintInfo();
-	void SetInfor(const string&name_, const string &id_, int age_, char gender_);
-	string GetName() { return name; }
-};
-
-class CUndergraduateStudent :public CStudent {
-private:
-	string department;
-public:
-	void QualifiedForBaoYan() {
-		cout << "qualified for baoyan" << endl;
-	}
-	void PrintInfo() {
-		CStudent::PrintInfo();
-		cout << "Department:" << department << endl;
-	}
-	void SetInfor(const string&name_, const string&id_, int age_, char gender_, const string & deparment_) {
-		CStudent::SetInfor(name_, id_, age_, gender_);
-		department = deparment_;
-	}
-};
-void CStudent::PrintInfo() {
-	cout << "Name:" << name << endl;
-	cout << "ID" << id << endl;
-	cout << "Age:" << age << endl;
-	cout << "Gender:" << gender << endl;
-}
-void CStudent::SetInfor(const string&name_, const string & id_, int age_, char gender_) {
-	name = name_;
-	id = id_;
-	age = age_;
-	gender = gender_;
+// 在此处补充你的代码
+int CompareString(const void * e1, const void * e2) {
+	MyString * s1 = (MyString *)e1;
+	MyString * s2 = (MyString *)e2;
+	if (*s1 < *s2)     return -1;
+	else if (*s1 == *s2) return 0;
+	else if (*s1 > *s2) return 1;
 }
 int main() {
-	CUndergraduateStudent s2;
-	s2.SetInfor("Harry Potter", "118829212", 19, 'M', "Computer Science");
-	cout << s2.GetName() << " ";
-	s2.QualifiedForBaoYan();
-	s2.PrintInfo();
+	MyString s1("abcd-"), s2, s3("efgh-"), s4(s1);
+	MyString SArray[4] = { "big","me","about","take" };
+	cout << "1. " << s1 << s2 << s3 << s4 << endl;
+	s4 = s3;    s3 = s1 + s3;
+	cout << "2. " << s1 << endl;
+	cout << "3. " << s2 << endl;
+	cout << "4. " << s3 << endl;
+	cout << "5. " << s4 << endl;
+	cout << "6. " << s1[2] << endl;
+	s2 = s1;    s1 = "ijkl-";
+	s1[2] = 'A';
+	cout << "7. " << s2 << endl;
+	cout << "8. " << s1 << endl;
+	s1 += "mnop";
+	cout << "9. " << s1 << endl;
+	s4 = "qrst-" + s2;
+	cout << "10. " << s4 << endl;
+	s1 = s2 + s4 + " uvw " + "xyz";
+	cout << "11. " << s1 << endl;
+	qsort(SArray, 4, sizeof(MyString), CompareString);
+	for (int i = 0; i < 4; ++i)
+		cout << SArray[i] << endl;
+	//输出s1从下标0开始长度为4的子串
+	cout << s1(0, 4) << endl;
+	//输出s1从下标为5开始长度为10的子串
+	cout << s1(5, 10) << endl;
 	return 0;
 }
