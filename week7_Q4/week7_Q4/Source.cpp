f599a643510a1827a5d2bb829ef93b1944f5fc81
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2019-01-08 19:37:01 -0800

    create new

--- week7_Q4/week7_Q4/Source.cpp
+++ week7_Q4/week7_Q4/Source.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
 
-int main() {
+
+int main()
+{
 	int n;
 	cin >> n;
-
+	string *str;
+	str = new string[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> str[i];
+	}
+	
 }
