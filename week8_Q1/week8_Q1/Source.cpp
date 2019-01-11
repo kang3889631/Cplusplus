b99e7df08945c046f21a1c8a9e6ef633f79798dd
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2019-01-10 19:01:23 -0800

    course example

--- week8_Q1/week8_Q1/Source.cpp
+++ week8_Q1/week8_Q1/Source.cpp
@@ -1,11 +1,21 @@
 #include <iostream> 
-#include <iterator> 
-#include <set> 
+#include <vector>
 using namespace std;
+
 int main() {
-	int a[] = { 8,7,8,9,6,2,1 };
-	// 在此处补充你的代码
-	ostream_iterator<int> o(cout, " ");
-	copy(v.begin(), v.end(), o);
+	vector<int>v;
+	v.push_back(1); v.push_back(2); v.push_back(3); v.push_back(4);
+	vector<int>::const_iterator i;
+	for (i = v.begin(); i != v.end(); ++i)
+		cout << *i << ",";
+	cout << endl;
+	vector<int>::reverse_iterator r;
+	for (r = v.rbegin(); r != v.rend(); r++)
+		cout << *r << endl;
+	vector<int>::iterator j;
+	for (j = v.begin(); j != v.end(); j++)
+		*j = 100;
+	for (i = v.begin(); i != v.end(); i++)
+		cout << *i << ",";
 	return 0;
 }
