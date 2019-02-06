ea88cca94564637a5bf8f62a6c660fd9ac534fd7
shiyin weng <kang3889631@gmail.com>
2019-02-06 13:11:23 -0800

    upgrade

---
+++ week10_Q8/week10_Q8/Source.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+// 在此处补充你的代码
+int main(int argc, char* argv[])
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, i, j;
+		cin >> n >> i >> j;
+		int result = 0;
+		if (n &(1 << i)) {//n的第i位为1，如果是0不需要改变
+			result |= 1 << i;
+		}
+		if (!(n & (1 << j)))//n的第j位为0,如果是1不需要改变
+		{
+			result |= 1 << j;
+		}
+		for (int k = i + 1; k < j; k++)
+		{
+			result |= 1 << k;
+		}
+		cout << hex << result << endl;
+	}
+	return 0;
+}
---
+++ week10_Q9/week10_Q9/Source.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <set>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+// 在此处补充你的代码
+class A {
+public:
+	int age;
+	char gender;
+	A(int k) :age(k), gender('A') {};
+	A(int k, char str) :age(k), gender(str) {};
+};
+class B:public A {
+public:
+	B(int k) :A(k, 'B') {};
+};
+struct Comp {
+	bool operator()(A* a1, A* a2) {
+		return a1->age < a2->age;
+	}
+};
+void Print(A* a) {
+	cout << a->gender << " " << a->age << endl;
+}
+int main()
+{
+
+	int t;
+	cin >> t;
+	set<A*, Comp> ct;
+	while (t--) {
+		int n;
+		cin >> n;
+		ct.clear();
+		for (int i = 0; i < n; ++i) {
+			char c; int k;
+			cin >> c >> k;
+
+			if (c == 'A')
+				ct.insert(new A(k));
+			else
+				ct.insert(new B(k));
+		}
+		for_each(ct.begin(), ct.end(), Print);
+		cout << "****" << endl;
+	}
+}
